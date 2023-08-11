352bbae9009c603e00d9993f70a784f6bd96f5fb
yassin chaddad <yassinchaddad@gmail.com>
2023-08-11 23:31:06 +0300

    Add admin dashboard user statistics

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Domains\Auth\Models\User;
+use Carbon\Carbon;
+
 /**
  * Class DashboardController.
  */
@@ -12,6 +15,29 @@ class DashboardController
      */
     public function index()
     {
-        return view('backend.dashboard');
+        $countData = User::count();
+
+        $typeCol = User::selectRaw('type as x, COUNT(*) as y')
+        ->orderBy("y")
+        ->groupBy('x')
+        ->get();
+
+        $typeData[] = [];
+        foreach ($typeCol as $value) {
+            $typeData[0][] = $value->x;
+        }
+        foreach ($typeCol as $value) {
+            $typeData[] = [$value->x, $value->y];
+        }
+
+
+        $dateCol = User::selectRaw('DATE(created_at) as x, COUNT(*) as y')
+        ->groupBy('x')
+        ->get();
+        $dateData[] = ['Date','Users Count'];
+        foreach ($dateCol as $value) {
+            $dateData[] = [$value->x, $value->y];
+        }
+        return view('backend.dashboard', compact('countData', 'dateData', 'typeData'));
     }
 }
